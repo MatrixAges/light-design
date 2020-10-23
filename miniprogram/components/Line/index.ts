417ea6a6d773dc7330d5f7f1a473f0b32dca0b8b
MatrixAge <xiewendao123@foxmail.com>
2020-10-23 17:59:18 +0800

    [add] component Line

---
+++ miniprogram/components/Line/index.ts
@@ -0,0 +1,32 @@
+Component({
+	properties: {
+		width: {
+			type: String,
+			value: '100%'
+		},
+		height: {
+			type: String,
+			value: '2rpx'
+		},
+		radius: {
+			type: Number,
+			value: 0
+		},
+		color: {
+			type: String,
+			value: 'black'
+		},
+		thin: {
+			type: Boolean,
+			value: false
+            },
+            box: {
+			type: Boolean,
+			value: false
+            },
+            borderSize: {
+			type: Number,
+			value: 2
+		},
+	}
+})
