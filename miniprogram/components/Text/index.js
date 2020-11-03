e9171f144f2ddc8efffe1a78f4a7764985e1a268
MatrixAge <xiewendao123@foxmail.com>
2020-11-03 18:14:21 +0800

    [add] Text

--- miniprogram/components/Text/index.js
+++ miniprogram/components/Text/index.js
@@ -4,14 +4,25 @@ Component({
         multipleSlots: true
     },
     properties: {
-        title: String,
-        width: String,
+        p: Boolean,
+        b: Boolean,
+        i: Boolean,
+        del: Boolean,
+        h1: Boolean,
+        h2: Boolean,
+        h3: Boolean,
+        h4: Boolean,
+        h5: Boolean,
+        h6: Boolean,
+        h7: Boolean,
+        quote: Boolean,
+        success: Boolean,
+        warning: Boolean,
+        error: Boolean,
         color: String,
-        bgColor: String,
-        bordered: Boolean,
-        noPadding: Boolean,
-        shadow: Boolean,
-        media: Boolean,
-        radius: Number
+        fontSize: String,
+        textAlign: String,
+        style: String,
+        lineClamp: Number
     }
 });
