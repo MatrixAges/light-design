ffe2b43cf1a904cf1cc74e48bd9c4ebd5ad34952
matrixAge <xiewendao123@foxmail.com>
2021-03-29 16:21:45 +0800

    [add] Period 时间预约选择器 制作完成

--- miniprogram/components/Period/index.ts
+++ miniprogram/components/Period/index.ts
@@ -1,3 +1,13 @@
+interface IDate {
+	raw: Date
+	text: string
+}
+
+interface ITime {
+	raw: { from: number; to: number }
+	text: string
+}
+
 Component({
 	properties: {
 		visible: {
@@ -11,6 +21,10 @@ Component({
 		zIndex: {
 			type: Number,
 			value: 100
+		},
+		showTwoHours: {
+			type: Boolean,
+			value: false
 		},
 		timeItems: {
 			type: Array,
@@ -27,9 +41,9 @@ Component({
 		_visible_mask: false,
 		active_date_index: 0,
 		active_time_index: 0,
-		date_items: [] as Array<string>,
-		time_items: [] as Array<string>,
-		time_items_raw: [] as Array<string>
+		date_items: [] as Array<IDate>,
+		time_items: [] as Array<ITime>,
+		time_items_raw: [] as Array<ITime>
 	},
 	observers: {
 		visible (new_val) {
@@ -70,31 +84,81 @@ Component({
 					return ''
 				}
 
-				_date_items.push(`${getDesc()}${day.getMonth() + 1}月${day.getDate()}日`)
+				const getWeek = () => {
+					if (i < 3) return ''
+
+					const week = day.getDay()
+
+					if (week === 0) return ' 周日'
+					if (week === 1) return ' 周一'
+					if (week === 2) return ' 周二'
+					if (week === 3) return ' 周三'
+					if (week === 4) return ' 周四'
+					if (week === 5) return ' 周五'
+					if (week === 6) return ' 周六'
+
+					return ''
+				}
+
+				const text = `${getDesc()}${day.getMonth() +
+					1}月${day.getDate()}日${getWeek()}`
+
+				_date_items.push({
+					raw: day,
+					text
+				})
 			}
 
 			_that.setData({
 				date_items: _date_items
 			})
-
-			_that.setTodayTimes()
 		},
-		'startTime,endTime,duration': function (startTime, endTime, duration){
+		'startTime,endTime,duration,timeItems': function (
+			startTime: number,
+			endTime: number,
+			duration: number,
+			timeItems: Array<string>
+		){
 			const _that = this
 
-			if (_that.data.time_items.length) return
+			if (timeItems.length) {
+				const _timeItems: Array<ITime> = []
 
-			const _time_items = []
-			const _time_items_raw = []
+				timeItems.map((item: string) => {
+					const from = Number(item.trim().split('-')[0].split(':')[0])
+					const to = Number(item.trim().split('-')[1].split(':')[1])
+
+					_timeItems.push({
+						raw: { from, to },
+						text: item
+					})
+                        })
+                        
+				_that.setData({
+					time_items: _timeItems,
+					time_items_raw: _timeItems.slice(0)
+				})
+
+				_that.setTodayTimes()
+
+				return
+			}
+
+			const _time_items: Array<{ raw: { from: number; to: number }; text: string }> = []
 
 			for (let i = startTime; i < endTime; i = i + duration) {
-				_time_items.push(`${i}:00 - ${i + duration}:00`)
-				_time_items_raw.push(`${i}:00 - ${i + duration}:00`)
+				_time_items.push({
+					raw: {
+						from: i,
+						to: i + duration
+					},
+					text: `${i}:00 - ${i + duration}:00`
+				})
 			}
 
 			_that.setData({
 				time_items: _time_items,
-				time_items_raw: _time_items_raw
+				time_items_raw: _time_items.slice(0)
 			})
 
 			_that.setTodayTimes()
@@ -105,10 +169,8 @@ Component({
 			if (new_val === 0) {
 				_that.setTodayTimes()
 			} else {
-				console.log(_that.data.time_items_raw)
-
 				_that.setData({
-					time_items: _that.data.time_items_raw
+					time_items: _that.data.time_items_raw.slice(0)
 				})
 			}
 
@@ -127,19 +189,23 @@ Component({
 
 			_that.data.time_items.map((item, index) => {
 				const start_time = new Date()
-				const time = item.trim().split('-')
+				const time = item.text.trim().split('-')
 
 				start_time.setHours(Number(time[0].split(':')[0]))
 
 				if (now > start_time.valueOf()) {
 					delete _that.data.time_items[index]
 				}
-			})
-
-			if (!_that.data.time_items.includes('两小时以内'))
-				_that.data.time_items.unshift('两小时以内')
-
+                  })
+                  
 			const _time_items = _that.data.time_items.filter(item => item)
+
+			if (
+				_time_items.findIndex(item => item.text === '两小时以内') === -1 &&
+				_that.data.showTwoHours
+			) {
+				_time_items.unshift({ raw: { from: 0, to: 0 }, text: '两小时以内' })
+			}
 
 			if (_time_items.length === 1) {
 				const start_time = new Date()
@@ -179,44 +245,11 @@ Component({
 				active_time_index
 			} = _that.data
 			const date = date_items[active_date_index]
-			const times = time_items[active_time_index]
-			const now = new Date()
-			const order_time_1 = new Date()
-			const order_time_2 = new Date()
-
-			let start_time: number | string = ''
-			let end_time: number | string = ''
-
-			switch (date.split(' ')[0]) {
-				case '今天':
-					break
-				case '明天':
-					order_time_1.setTime(now.getTime() + 24 * 60 * 60 * 1000)
-					order_time_2.setTime(now.getTime() + 24 * 60 * 60 * 1000)
-					break
-				case '后天':
-					order_time_1.setTime(now.getTime() + 24 * 60 * 60 * 1000 * 2)
-					order_time_2.setTime(now.getTime() + 24 * 60 * 60 * 1000 * 2)
-					break
-			}
-
-			if (times !== '两小时以内') {
-				order_time_1.setHours(Number(times.split('-')[0].split(':')[0]))
-				order_time_2.setHours(Number(times.split('-')[1].split(':')[0]))
-				order_time_1.setMinutes(0)
-				order_time_2.setMinutes(0)
-				order_time_1.setSeconds(0)
-				order_time_2.setSeconds(0)
-
-				start_time = parseInt(String(order_time_1.valueOf() / 1000))
-				end_time = parseInt(String(order_time_2.valueOf() / 1000))
-			}
+			const time = time_items[active_time_index]
 
 			_that.triggerEvent('onSelectedPeriod', {
-				start_time: start_time,
-				end_time: end_time,
-				date: date,
-				times: times
+				date,
+				time
 			})
 
 			_that.onClose()
--- miniprogram/pages/Period/index.ts
+++ miniprogram/pages/Period/index.ts
@@ -3,7 +3,9 @@ import { blockRight } from 'components/Icon/data'
 Page({
 	data: {
 		blockRight: blockRight,
-		visible_default: false
+		visible_default: false,
+		visible_period_data: false,
+		period_data: {}
 	},
 	onOption (e: any) {
 		const _that = this
@@ -13,6 +15,14 @@ Page({
 
 		_that.setData({
 			[`visible_${type}`]: true
+		})
+	},
+	onSelectedPeriod ({ detail }: any) {
+		const _that = this
+
+		_that.setData({
+			visible_period_data: true,
+			period_data: JSON.parse(JSON.stringify(detail))
 		})
 	}
 })
